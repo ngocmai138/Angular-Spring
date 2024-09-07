7f62e91dda0205dbbca1d320bbf30e950fb0aab9
ngocmai138 <nnm0891@gmail.com>
2024-09-07 13:59:04 +0700

    24

--- todo/src/app/wellcome/welcome.component.ts
+++ todo/src/app/wellcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import {ActivatedRoute, RouterLink} from "@angular/router";
+import {WelcomeDataService} from "../service/data/welcome-data.service";
 
 @Component({
   selector: 'app-welcome',
@@ -10,10 +11,15 @@ import {ActivatedRoute, RouterLink} from "@angular/router";
 })
 export class WelcomeComponent implements OnInit{
   name=''
-  constructor(private route:ActivatedRoute) {
+  constructor(private route:ActivatedRoute,
+              private service:WelcomeDataService) {
   }
 
   ngOnInit(): void {
     this.name=this.route.snapshot.params['name']
+  }
+
+  getWelcomeMessage() {
+    this.service.executeHelloWorldBeanService()
   }
 }
